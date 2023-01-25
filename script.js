92338e7b1fe5ca842f6cea02200d69582f82eb71
Éric Nascimento <nascimento3ric@gmail.com>
2023-01-25 17:11:38 -0300

    versão 1.0, máscara de texto funcionando

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 
   let article = "Alan Mathison Turing (Londres, 23 de junho de 1912 — Wilmslow, Cheshire, 7 de junho de 1954) foi um matemático cientista da computação, lógico, criptoanalista, filósofo e biólogo teórico britânico. Turing foi altamente influente no desenvolvimento da moderna ciência da computação teórica, proporcionando uma formalização dos conceitos de algoritmo e computação com a máquina de Turing, que pode ser considerada um modelo de um computador de uso geral. Ele é amplamente considerado o pai da ciência da computação teórica e da inteligência artificial. Apesar dessas realizações ele nunca foi totalmente reconhecido em seu país de origem durante sua vida por ser homossexual e porque grande parte de seu trabalho foi coberto pela Lei de Segredos Oficiais"
   
-  $mask = "&dagger;";
+  $mask = "■";
+
+  var title = "Alan Mathison Turing"
+  var titleArray = title.split(" ");
   var articleArrayLower = removeAccent(article).split(" ");
   var articleArray = article.split(" ");
+  
   var auxArray = maskInText(articleArray);
+  var auxTitle = maskInText(titleArray);
   
   
   
   
-  function removeAccent(article){
+    function removeAccent(article){
       let withAccent = "ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝŔÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿŕ";
       let withoutAccent = "AAAAAAACEEEEIIIIDNOOOOOOUUUUYRsBaaaaaaaceeeeiiiionoooooouuuuybyr";
       let auxWord = "";
@@ -21,46 +26,67 @@
             } else {
                 auxWord += char;
             }
-        }
-        return auxWord; 
+        } 
+
+        return auxWord.toLowerCase();
+
     }    
     
-    function changeWord(word){ 
-    var wordMasked = '';
-    for(var i = 0; i < word.length; i++){
-        wordMasked = wordMasked + $mask
-    }
-    return wordMasked;
-    }
-    
     function maskInText(articleArray){
-    var articleMasked = [];
-
-    for(var i = 0; i < articleArray.length; i++){
-        articleMasked[i] = changeWord(articleArray[i])
-    }
+        var articleMasked = [];
+        
+        for(var i = 0; i < articleArray.length; i++){
+        var wordMasked = '';
+            for(var j = 0; j < articleArray[i].length; j++){
+                wordMasked = wordMasked + $mask
+                articleMasked[i] = wordMasked
+            }
+        }
 
     return articleMasked;
     }
 
-   function fetchInArticle(){
-        var keyWord = document.getElementById("inputSearchWord").value;
-
+    function fetchInArticle(){
+        var keyWord = document.getElementById("inputSearchWord").value;       
+        
         for(var i = 0; i < articleArray.length; i++){
             
-            if(articleArray[i] == keyWord){
-
-
+            if(removeAccent(articleArray[i]) == removeAccent(keyWord)){
+                
                 console.log(articleArray[i] + " na Posição " + i)
-
                 auxArray[i] = articleArray[i]
-                document.getElementById("artigoId").innerHTML = auxArray.join(" ");            
-            
+                document.getElementById("artigoId").innerHTML = auxArray.join(" ");       
+                // document.getElementById("alertRight").innerHTML = "Boa!!";     
+                document.getElementById("inputSearchWord").value = "";
+
+            }
+        }
+
+        
+        for(var i = 0; i < titleArray.length; i++){
+            if(removeAccent(titleArray[i]) == removeAccent(keyWord)){
+                auxTitle[i] = titleArray[i];
+
+                document.getElementById("titleId").innerHTML = auxTitle.join(" ");
             }
         }
     }
     
+// simula click em button quando Enter é pressionado
+    var button = document.getElementById("buttonSearch");
+    document.addEventListener("keydown", function(event) { 
+      if (event.keyCode === 13) {
+        button.click();
+      }
+    });
+
     
     document.getElementById("artigoId").innerHTML = maskInText(articleArray).join(" ");
+    document.getElementById("titleId").innerHTML = maskInText(titleArray).join(" ");
 
 
+
+
+        
+
+   
