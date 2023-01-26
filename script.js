a8e5fd9560d17f67bb1654f2fe423a0a541f252a
Éric Nascimento <nascimento3ric@gmail.com>
2023-01-26 12:06:51 -0300

    celebrate adicionado

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
-  let article = "Alan Mathison Turing (Londres, 23 de junho de 1912 — Wilmslow, Cheshire, 7 de junho de 1954) foi um matemático cientista da computação, lógico, criptoanalista, filósofo e biólogo teórico britânico. Turing foi altamente influente no desenvolvimento da moderna ciência da computação teórica, proporcionando uma formalização dos conceitos de algoritmo e computação com a máquina de Turing, que pode ser considerada um modelo de um computador de uso geral. Ele é amplamente considerado o pai da ciência da computação teórica e da inteligência artificial. Apesar dessas realizações ele nunca foi totalmente reconhecido em seu país de origem durante sua vida por ser homossexual e porque grande parte de seu trabalho foi coberto pela Lei de Segredos Oficiais"
+let article = "Alan Mathison Turing (Londres, 23 de junho de 1912 — Wilmslow, Cheshire, 7 de junho de 1954) foi um matemático cientista da computação, lógico, criptoanalista, filósofo e biólogo teórico britânico. Turing foi altamente influente no desenvolvimento da moderna ciência da computação teórica, proporcionando uma formalização dos conceitos de algoritmo e computação com a máquina de Turing, que pode ser considerada um modelo de um computador de uso geral. Ele é amplamente considerado o pai da ciência da computação teórica e da inteligência artificial. Apesar dessas realizações ele nunca foi totalmente reconhecido em seu país de origem durante sua vida por ser homossexual e porque grande parte de seu trabalho foi coberto pela Lei de Segredos Oficiais"
   
   $mask = "■";
+// $mask = "|";
 
   var title = "Alan Mathison Turing"
   var titleArray = title.split(" ");
@@ -58,10 +59,12 @@
                 document.getElementById("artigoId").innerHTML = auxArray.join(" ");       
                 // document.getElementById("alertRight").innerHTML = "Boa!!";     
                 document.getElementById("inputSearchWord").value = "";
-
+                jsConfetti.addConfetti()
             }
+            
         }
-
+        
+        
         
         for(var i = 0; i < titleArray.length; i++){
             if(removeAccent(titleArray[i]) == removeAccent(keyWord)){
@@ -81,12 +84,22 @@
     });
 
     
+    
     document.getElementById("artigoId").innerHTML = maskInText(articleArray).join(" ");
     document.getElementById("titleId").innerHTML = maskInText(titleArray).join(" ");
 
 
+    const canvas = document.querySelector('#celebrate');
+    
+    const jsConfetti = new JSConfetti();
 
-
-        
-
+    
+     
+    
    
+    
+    
+
+    
+
+    
